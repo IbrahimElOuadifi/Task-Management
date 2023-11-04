be18c56b67f52002dce5400e775299a93ce486bd
Ibrahim El Ouadifi <elouadifi.ibrahim@gmail.com>
2023-11-05 00:38:47 +0100

    update user db filtering

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -23,14 +23,14 @@ export const register = async (req: Request, res: Response) => {
         const { firstName, lastName, username, password, confirmPassword } = req.body
         await registerSchema.validate({ firstName, lastName, username, password, confirmPassword })
         if (password !== confirmPassword) return res.status(400).json({ message: 'Passwords do not match' })
-        const user = await User.findOne({ username })
+        const user = await User.findOne({ username: { $regex: username.trim(), $options: 'i' } })
         if (user) return res.status(400).json({ message: 'User already exists' })
         const salt = await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(password, salt)
         const newUser = new User({
             firstName,
             lastName,
-            username,
+            username: username.trim(),
             password: hashedPassword,
         })
         const resp = await newUser.save()
@@ -47,7 +47,7 @@ export const login = async (req: Request, res: Response) => {
         console.log(req.body, process.env.JWT_SECRET_KEY)
         const { username, password } = req.body
         await loginSchema.validate({ username, password })
-        const user = await User.findOne({ username })
+        const user = await User.findOne({ username: { $regex: username.trim(), $options: 'i' } })
         if (!user) return res.status(400).json({ message: 'User does not exist' })
         const isMatch = await bcrypt.compare(password, user.password)
         if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' })
