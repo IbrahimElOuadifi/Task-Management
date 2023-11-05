ca570726cf9ca8406fa9959c473bf2aa3505551c
Ibrahim El Ouadifi <elouadifi.ibrahim@gmail.com>
2023-11-05 21:11:59 +0100

    create project dialog

--- client/src/pages/private/projects/list/new-project.tsx
+++ client/src/pages/private/projects/list/new-project.tsx
@@ -1,38 +1,68 @@
-import { FC } from 'react'
+import { useState, FC } from 'react'
 import { Card, CardHeader } from '@components/ui/card'
+import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogFooter } from '@components/ui/dialog'
+import { Button } from '@components/ui/button'
+import { Input } from '@components/ui/input'
 import { createProject } from 'api/project'
-import { useSelector } from 'react-redux'
+import { usePOSTData } from 'hooks/index'
+import { useForm, Controller } from 'react-hook-form'
 
 export interface NewProjectProps {
     onSuccessfulCreate?: () => void,
     onFailedCreate?: () => void
 }
 
-const NewProject: FC<NewProjectProps> = ({ onSuccessfulCreate }) => {
+const NewProject: FC<NewProjectProps> = ({ onSuccessfulCreate, onFailedCreate }) => {
 
-    const { token } = useSelector((state: any) => state.auth)
 
-    const handleClick = async () => {
-        try {
-            const name = prompt('Enter project name') as string
-            if (name) {
-                const resp = await createProject({ name, token })
-                if (resp) {
-                    onSuccessfulCreate && onSuccessfulCreate()
-                }
-            }
-        } catch (error) {
-            console.log(error)
+    const [dialogOpen, setDialogOpen] = useState(false)
+
+    const { handleSubmit, control, formState: { errors } } = useForm({
+        defaultValues: {
+            name: ''
         }
+    })
+
+    const { postData } = usePOSTData<{ name: string }>(createProject, onSuccessfulCreate, onFailedCreate)
+
+    const onSubmit = ({ name }: { name: string }) => {
+        postData({ name }).then(() => setDialogOpen(false))
+    }
+
+    const onError = (errors: any) => {
+        console.log(errors)
     }
 
     return (
         <div className='w-full h-28 md:px-2 mb-4 lg:w-1/3 xl:w-1/4 2xl:w-1/5'>
-            <Card className='h-full rounded-sm mb-4 cursor-pointer hover:bg-gray-50 hover:shadow-md' onClick={handleClick}>
-                <CardHeader className='flex justify-center align-middle h-full text-center'>
-                    <h2>CREATE NEW</h2>
-                </CardHeader>
-            </Card>
+            <Dialog open={dialogOpen} onOpenChange={open => setDialogOpen(open)}>
+                <DialogTrigger asChild>
+                    <Card className='h-full rounded-sm mb-4 cursor-pointer hover:bg-gray-50 hover:shadow-md'>
+                        <CardHeader className='flex justify-center align-middle h-full text-center'>
+                            <h2>CREATE NEW</h2>
+                        </CardHeader>
+                    </Card>
+                </DialogTrigger>
+                <DialogContent className='sm:max-w-[425px]'>
+                    <DialogHeader>
+                        <DialogTitle>New Project</DialogTitle>
+                        <hr className='my-2' />
+                    </DialogHeader>
+                        <form onSubmit={handleSubmit(onSubmit, onError)} className='flex flex-col gap-4'>
+                            <Controller
+                                name='name'
+                                control={control}
+                                rules={{ required: true }}
+                                render={({ field }) => (
+                                    <Input {...field} placeholder='Project name' error={Boolean(errors.name)} />
+                                )}
+                            />
+                            <Button type='submit'>Submit</Button>
+                        </form>
+                    <DialogFooter>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
         </div>
     )
 }
