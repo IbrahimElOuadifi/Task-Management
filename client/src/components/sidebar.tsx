6249936336747696c04a396b5d9de60cc3d8da10
Ibrahim El Ouadifi <elouadifi.ibrahim@gmail.com>
2023-11-05 17:02:52 +0100

    update sidebar style

--- client/src/components/sidebar.tsx
+++ client/src/components/sidebar.tsx
@@ -1,25 +1,29 @@
 import { FC } from 'react'
 import { NavLink } from 'react-router-dom'
 
+const Link = ({ children, ...props }: any) => {
+    return (
+        <li>
+            <NavLink 
+                {...props}
+                className={({ isActive }) => `text-lg transition-all ease-in-out duration-150 text-gray-600 animate-in hover:text-gray-900 ${isActive ? '' : ''}`}
+                children={({ isActive }) => (
+                    <div className={`rounded-md px-4 py-1.5 ${isActive ? 'bg-gray-100' : 'hover:bg-gray-50'}`}>
+                        {children}
+                    </div>
+                )}
+            />
+        </li>
+    )
+}
+
 const SideBar: FC = () => {
     return (
         <div className='h-full py-4 px-2'>
             <ul className='space-y-2'>
-                <li>
-                    <NavLink to='/' className={({ isActive }) => `text-gray-600 hover:text-gray-900 hover:font-bold ${isActive ? 'font-bold' : ''}`}>
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/projects' className={({ isActive }) => `text-gray-600 hover:text-gray-900 hover:font-bold ${isActive ? 'font-bold' : ''}`}>
-                        Projects
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/about' className={({ isActive }) => `text-gray-600 hover:text-gray-900 hover:font-bold ${isActive ? 'font-bold' : ''}`}>
-                        About
-                    </NavLink>
-                </li>
+                <Link to='/'>Home</Link>
+                <Link to='/projects'>Projects</Link>
+                <Link to='/about'>About</Link>
             </ul>
         </div>
     )
