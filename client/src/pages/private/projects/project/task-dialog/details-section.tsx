b22317d4c09b5f7f157877076c3e29f193bdbd75
Ibrahim El Ouadifi <elouadifi.ibrahim@gmail.com>
2023-12-02 23:34:49 +0100

    add text details

--- client/src/pages/private/projects/project/task-dialog/details-section.tsx
+++ client/src/pages/private/projects/project/task-dialog/details-section.tsx
@@ -1,16 +1,70 @@
-import { FC } from 'react'
+import { useState, useRef, FC, useEffect } from 'react'
+import { Button } from '@components/ui/button'
 import { Textarea } from '@components/ui/textarea'
-import { ITask } from '@interfaces/Task'
+import { useForm, Controller } from 'react-hook-form'
+import { useOutsideAlerter, usePOSTData } from 'hooks/index'
+import { ITask, updateTaskDescriptionOptions } from '@interfaces/Task'
+import { updateTaskDescription } from 'api/task'
 
 interface DetailsSectionProps {
-    task: ITask | null
+    task: ITask | null,
+    onSave: (description: string) => void,
 }
 
-const DetailsSection:FC<DetailsSectionProps> = ({ task }) => {
+const DetailsSection:FC<DetailsSectionProps> = ({ task, onSave }) => {
+
+        const [isEdit, setEdit] = useState<boolean>(false)
+
+        const ref = useRef(null)
+
+        const { handleSubmit, control, setValue } = useForm<{ description: string }>({
+            defaultValues: {
+                description: ''
+            }
+        })
+
+        const { postData: postUpdateTaskDescription } = usePOSTData<updateTaskDescriptionOptions>(updateTaskDescription, onSave, onSave)
+
+        const onSubmit = (data: { description: string }) => {
+            postUpdateTaskDescription({ ...data, id: task?._id || '' })
+            .then(() => setEdit(false))
+            .catch((err) => console.log(err))
+        }
+        
+        const handleClose = () => {
+            setEdit(false)
+            setValue('description', task?.description || '')
+        }
+        
+        useOutsideAlerter(ref, handleClose)
+
+        useEffect(() => {
+            if(task) setValue('description', task.description)
+        }, [task])
+
     return (
-        <div className='flex flex-row gap-2'>
-            <Textarea className='text-sm resize-none cursor-pointer' rows={4} value={task?.description || ''} readOnly placeholder='Add a more detailed description...' />
-        </div>
+        <form className='flex flex-col gap-2' ref={ref} onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+                name='description'
+                control={control}
+                render={({ field }) => (
+                    <Textarea
+                        {...field}
+                        className={`text-sm resize-none ${isEdit ? 'cursor-text' : 'cursor-pointer'}`}
+                        rows={isEdit ? 5 : 3}
+                        readOnly={!isEdit}
+                        onClick={() => setEdit(true)}
+                        placeholder='Add a more detailed description...' />
+                )} />
+                    
+                {
+                    isEdit &&
+                    <div className='flex justify-end items-center space-x-1'>
+                        <Button variant='outline' className='text-sm' type='button' onClick={handleClose}>Cancel</Button>
+                        <Button variant='default' className='text-sm' type='submit'>Save</Button>
+                    </div>
+                }
+        </form>
     )
 }
 
