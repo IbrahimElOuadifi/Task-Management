8d33725f6a23ed4c2a68fb12303526d0f51dd7a3
Ibrahim El Ouadifi <elouadifi.ibrahim@gmail.com>
2024-01-27 18:28:50 +0100

    update vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,21 +10,21 @@ export default defineConfig({
     react(),
     tsconfigPaths()
   ],
-  // resolve: {
-  //   alias: {
-  //     "@": path.resolve(__dirname, "./src"),
-  //     "@components": path.resolve(__dirname, "./src/components"),
-  //     "@layouts": path.resolve(__dirname, "./src/layouts"),
-  //     "hooks": path.resolve(__dirname, "./src/hooks"),
-  //     "pages": path.resolve(__dirname, "./src/pages"),
-  //     "api": path.resolve(__dirname, "./src/api"),
-  //     "@assets": path.resolve(__dirname, "./src/assets"),
-  //     "@interfaces": path.resolve(__dirname, "./src/interfaces"),
-  //     "@utils": path.resolve(__dirname, "./src/lib/utils"),
-  //     "@store-actions": path.resolve(__dirname, "./src/redux"),
-  //     "@contexts": path.resolve(__dirname, "./src/contexts"),
-  //   },
-  // },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@layouts": path.resolve(__dirname, "./src/layouts"),
+      "hooks": path.resolve(__dirname, "./src/hooks"),
+      "pages": path.resolve(__dirname, "./src/pages"),
+      "api": path.resolve(__dirname, "./src/api"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@interfaces": path.resolve(__dirname, "./src/interfaces"),
+      "@utils": path.resolve(__dirname, "./src/lib/utils"),
+      "@store-actions": path.resolve(__dirname, "./src/redux"),
+      "@contexts": path.resolve(__dirname, "./src/contexts"),
+    },
+  },
   build: {
     outDir: './dist',
     emptyOutDir: true,
